efdccb36c647b55c1aa502d098d47177f639f12f
“manusmd” <manuel.schmid@js-tut.de>
2021-10-22 15:16:13 +0200

    :bug: Add async function

--- components/mainElement.js
+++ components/mainElement.js
@@ -2,13 +2,11 @@ import createElement from '../lib/element';
 import styles from './mainElement.module.css';
 import createCharacterCard from './characterCard';
 
-export default function createMainElement() {
-  const characterCard = createCharacterCard(
-    'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-    'Walter White',
-    'Heisenberg',
-    'Deseased',
-    '12345'
-  );
-  return createElement('div', { className: styles.main }, [characterCard]);
+export default async function createMainElement() {
+  const response = await fetch('https://breakingbadapi.com/api/characters');
+  const body = await response.json();
+  const characters = body.map((character) => {
+    return createCharacterCard(character);
+  });
+  return createElement('div', { className: styles.main }, [...characters]);
 }

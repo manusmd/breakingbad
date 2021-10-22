79e1aa2f8df07ede8c22e5e0a92469892d05a5ed
“manusmd” <manuel.schmid@js-tut.de>
2021-10-22 15:30:44 +0200

    :bug: Add async function

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import './style.css';
 import createHeaderElement from './components/headerComponent';
 import createMainElement from './components/mainElement';
 
-function renderApp() {
+async function renderApp() {
   const appElement = document.querySelector('#app');
   const headerElement = createHeaderElement();
-  const mainElement = createMainElement();
+  const mainElement = await createMainElement();
 
   appElement.append(headerElement, mainElement);
 }

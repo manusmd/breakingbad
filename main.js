55d58b6149dc2abbf07009fba2d1e737cccb730b
“manusmd” <manuel.schmid@js-tut.de>
2021-10-22 17:33:28 +0200

    :bug: Add an import

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import './style.css';
 import createHeaderElement from './components/headerComponent';
+import createRandomQuote from './components/randomQuote';
 import createMainElement from './components/mainElement';
 
 async function renderApp() {
   const appElement = document.querySelector('#app');
   const headerElement = createHeaderElement();
+  const randomQuote = await createRandomQuote();
   const mainElement = await createMainElement();
 
-  appElement.append(headerElement, mainElement);
+  appElement.append(headerElement, randomQuote, mainElement);
 }
 renderApp();

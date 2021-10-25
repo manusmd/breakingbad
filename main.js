56a09dbddc25273b5b17c07639bcfce544830b92
“manusmd” <manuel.schmid@js-tut.de>
2021-10-25 11:13:23 +0200

    :sparkles: Add scroll to top button

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ import './style.css';
 import createHeaderElement from './components/headerComponent';
 import createRandomQuote from './components/randomQuote';
 import createMainElement from './components/mainElement';
+import createScrollButton from './components/scrollTop';
 
 async function renderApp() {
   const appElement = document.querySelector('#app');
   const headerElement = createHeaderElement();
   const randomQuote = await createRandomQuote();
   const mainElement = await createMainElement();
+  const scrollTopButton = createScrollButton();
 
-  appElement.append(headerElement, randomQuote, mainElement);
+  appElement.append(headerElement, randomQuote, mainElement, scrollTopButton);
 }
 renderApp();
